070a5f3f1885186c450c93605aa7d71a02204602
shaobo <969781695@qq.com>
2018-01-12 23:17:28 +0800

    添加标题c3动画

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 /**
  * Created by shaobo on 2018/1/12.
  */
-$(function(){
+$(function () {
   $('#fullpage').fullpage({
     navigation: true
   });
+  //标题动画
+  var $title = $(".section .title");
+  $title.on("mouseenter",function(){
+    $(this).children("h6").addClass("active").removeClass("leave");
+  });
+  $title.on("mouseleave",function(){
+    $(this).children("h6").addClass("leave").removeClass("active");
+
+  })
 });
